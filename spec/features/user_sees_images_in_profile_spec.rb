2e4e1f5672f8ad67fad5a3e4930064247fd379a3
gionaufal <g10v45+github@gmail.com>
2016-10-01 14:22:12 -0300

    WIP images in profile page

---
+++ spec/features/user_sees_images_in_profile_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'User sees images in profile' do
+  scenario 'successfully' do
+    user = create(:user)
+    login_as(user)
+
+    visit root_path
+
+    click_on 'New post'
+
+    post = build(:post, user: user)
+
+    fill_in 'Description',      with: post.description
+    attach_file('post_image', 'app/assets/images/inktober.png')
+
+    click_on 'Create post'
+
+    visit "#{root_path}/users/users/#{user.username}"
+    expect(current_path).to eq("/users/#{user.username}")
+    expect(page).to have_css("img[src*='inktober.png']")
+    expect(page).to have_content post.description
+  end
+end
